40c7cd5872e173ee77a6def2dd3e0be004afccab
Verdysh Volodymyr <verdyshvladimir@gmail.com>
2025-03-03 21:57:19 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (e) => {
+  event.preventDefault();
+
+  if (e.target.alt === 'thumb') {
+    const arr = e.target.closest('a');
+
+    gallery.children[0].children[0].attributes.src.value = arr.href;
+
+    return;
+  }
+
+  gallery.children[0].children[0].attributes.src.value = e.target.href;
+});
