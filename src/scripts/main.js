db94765c50ad9ac1de9a825ab2231141f8c49bcf
Verdysh Volodymyr <verdyshvladimir@gmail.com>
2025-03-03 22:05:29 +0200

    fixed bugs for comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,14 @@ const gallery = document.querySelector('.gallery');
 const galleryList = document.querySelector('.gallery__list');
 
 galleryList.addEventListener('click', (e) => {
-  event.preventDefault();
+  e.preventDefault();
 
   if (e.target.alt === 'thumb') {
-    const arr = e.target.closest('a');
+    const anchorElement = e.target.closest('a');
 
-    gallery.children[0].children[0].attributes.src.value = arr.href;
+    if (anchorElement) {
+      gallery.children[0].children[0].attributes.src.value = anchorElement.href;
+    }
 
     return;
   }
